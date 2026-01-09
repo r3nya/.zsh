65bfbf36d67f9d2f7a055c68fd0a385f898ade6c
Andrew Makarov <me@r3nya.ru>
2026-01-09 22:52:01 +0100

    Add Zsh completion system with flexible matching and styling
    Initialize the Zsh completion system with case-insensitive and substring
    matching, along with menu selection styling.

--- modules/01-core.zsh
+++ modules/01-core.zsh
@@ -1,6 +1,22 @@
 autoload -U colors && colors
 setopt PROMPT_SUBST
 
+# Completion system
+autoload -Uz compinit && compinit -u
+
+# Flexible completion matching
+# - Case insensitive matching
+# - Substring matching (match anywhere in the word)
+# - Partial word completion
+zstyle ':completion:*' matcher-list '' \
+  'm:{a-zA-Z}={A-Za-z}' \
+  'r:|[._-]=* r:|=*' \
+  'l:|=* r:|=*'
+
+# Completion menu styling
+zstyle ':completion:*' menu select
+zstyle ':completion:*' list-colors ''
+
 HISTFILE=~/.zsh_history
 HISTSIZE=50000
 SAVEHIST=50000
